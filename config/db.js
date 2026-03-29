06ad144add9eda89b1e7ece893e0bbd65f5ad79a
Hieu <hieeuspham9@gmail.com>
2026-03-29 16:16:05 +0700

    fix db to better-sqlite3

--- config/db.js
+++ config/db.js
@@ -1,14 +1,16 @@
 
+const Database = require('better-sqlite3');
 const path = require('path');
 
 const dbPath = path.join(__dirname, '..', 'subvn.db');
 
-const db = new sqlite3.Database(dbPath, (err) => {
-  if (err) {
-    console.error('❌ DB connection error', err);
-  } else {
-    console.log('✅ SQLite connected');
-  }
-});
+let db;
+
+try {
+  db = new Database(dbPath);
+  console.log('✅ SQLite (better-sqlite3) connected');
+} catch (err) {
+  console.error('❌ DB connection error', err);
+}
 
 module.exports = db;
